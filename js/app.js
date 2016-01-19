96dfe489d67839818b8c78cc7578ed6b104af435
kmolumby <kelly.molumby@gmail.com>
2016-01-18 19:16:17 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+	$(".list").on("click", "button", function (event) { 
+
+	
+
+		for ( var i=1; i<=100; i++) {
+
+		if ( i % 3 === 0 && i % 5 === 0) {
+			$(".list").append("<p>FizzBuzz</p>");
+		}
+
+		else if (i % 3 === 0) {
+			$(".list").append("<p>Fizz</p>");
+		}
+
+		else if (i % 5 === 0) {
+			$(".list").append("<p>Buzz</p>")
+		}
+
+		else {
+			$(".list").append("<p>"+i+"</p>")
+		}
+
+
+
+		};
+
+	});
+
+});
